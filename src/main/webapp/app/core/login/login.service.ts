c537c2bd6c452fad4b5e9c2e6d53981dae25fdd2
fokkog <fokko_groenenboom@hotmail.com>
2020-05-24 15:30:57 +0200

    AAD B2C support (initial/incomplete)

--- src/main/webapp/app/core/login/login.service.ts
+++ src/main/webapp/app/core/login/login.service.ts
@@ -11,7 +11,7 @@ export class LoginService {
   login(): void {
     // If you have configured multiple OIDC providers, then, you can update this URL to /login.
     // It will show a Spring Security generated login page with links to configured OIDC providers.
-    location.href = `${location.origin}${this.location.prepareExternalUrl('oauth2/authorization/oidc')}`;
+    location.href = `${location.origin}${this.location.prepareExternalUrl('oauth2/authorization/azure')}`;
   }
 
   logout(): void {
