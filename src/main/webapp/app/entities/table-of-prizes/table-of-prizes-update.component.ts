87fc90ecce05396cc9d313b5d6ff140fa1c4c9a8
fokkog <fokko_groenenboom@hotmail.com>
2020-07-04 14:20:38 +0200

    Intermediate commit for top

--- src/main/webapp/app/entities/table-of-prizes/table-of-prizes-update.component.ts
+++ src/main/webapp/app/entities/table-of-prizes/table-of-prizes-update.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpResponse } from '@angular/common/http';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormArray, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { ITableOfPrizes, TableOfPrizes } from 'app/shared/model/table-of-prizes.model';
 import { TableOfPrizesService } from './table-of-prizes.service';
+import { IPrize, Prize } from 'app/shared/model/prize.model';
 
 @Component({
   selector: 'jhi-table-of-prizes-update',
@@ -15,26 +16,61 @@ import { TableOfPrizesService } from './table-of-prizes.service';
 export class TableOfPrizesUpdateComponent implements OnInit {
   isSaving = false;
 
-  editForm = this.fb.group({
-    id: [],
-    name: [null, [Validators.required, Validators.maxLength(100)]],
-    userId: [],
-  });
+  editForm = this.fb.group({});
 
   constructor(protected tableOfPrizesService: TableOfPrizesService, protected activatedRoute: ActivatedRoute, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ tableOfPrizes }) => {
-      this.updateForm(tableOfPrizes);
+      this.createViewFromModel(tableOfPrizes);
     });
   }
 
-  updateForm(tableOfPrizes: ITableOfPrizes): void {
-    this.editForm.patchValue({
-      id: tableOfPrizes.id,
-      name: tableOfPrizes.name,
-      userId: tableOfPrizes.userId,
+  createViewFromModel(tableOfPrizes: ITableOfPrizes): void {
+    const rows = this.fb.array([]);
+    if (tableOfPrizes.prizes) {
+      for (let i = 0; i < tableOfPrizes.prizes.length; i++) {
+        rows.push(this.createViewRowFromModelChild(tableOfPrizes.prizes[i]));
+      }
+    }
+    this.editForm = this.fb.group({
+      id: [tableOfPrizes.id],
+      name: [tableOfPrizes.name, [Validators.required, Validators.maxLength(100)]],
+      userId: [tableOfPrizes.userId],
+      prizes: rows,
     });
+  }
+
+  private createViewRowFromModelChild(prize: IPrize): FormGroup {
+    return this.fb.group({
+      id: [prize.id],
+      quantity: [prize.quantity, [Validators.required, Validators.min(1)]],
+      imageId: [prize.image?.id],
+      imageName: [prize.image?.name],
+      imageUrl: [prize.image?.url],
+    });
+  }
+
+  getViewRows(): FormArray {
+    return this.editForm.get('prizes') as FormArray;
+  }
+
+  addViewRow(): void {
+    this.getViewRows().push(this.createViewRowFromModelChild(new Prize()));
+  }
+
+  deleteViewRow(index: number): void {
+    this.getViewRows().removeAt(index);
+  }
+
+  private createModelFromView(): ITableOfPrizes {
+    return {
+      ...new TableOfPrizes(),
+      id: this.editForm.get('id')!.value,
+      name: this.editForm.get('name')!.value,
+      userId: this.editForm.get('userId')!.value,
+      prizes: this.editForm.get('prizes')!.value,
+    };
   }
 
   previousState(): void {
@@ -43,21 +79,12 @@ export class TableOfPrizesUpdateComponent implements OnInit {
 
   save(): void {
     this.isSaving = true;
-    const tableOfPrizes = this.createFromForm();
-    if (tableOfPrizes.id !== undefined) {
+    const tableOfPrizes = this.createModelFromView();
+    if (tableOfPrizes.id) {
       this.subscribeToSaveResponse(this.tableOfPrizesService.update(tableOfPrizes));
     } else {
       this.subscribeToSaveResponse(this.tableOfPrizesService.create(tableOfPrizes));
     }
-  }
-
-  private createFromForm(): ITableOfPrizes {
-    return {
-      ...new TableOfPrizes(),
-      id: this.editForm.get(['id'])!.value,
-      name: this.editForm.get(['name'])!.value,
-      userId: this.editForm.get(['userId'])!.value,
-    };
   }
 
   protected subscribeToSaveResponse(result: Observable<HttpResponse<ITableOfPrizes>>): void {
