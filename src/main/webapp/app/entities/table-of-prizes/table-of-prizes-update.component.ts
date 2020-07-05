89fb9c9f288c394df250f990b2462916f6909458
fokkog <fokko_groenenboom@hotmail.com>
2020-07-05 12:24:37 +0200

    Front-end editing of prizes on table

--- src/main/webapp/app/entities/table-of-prizes/table-of-prizes-update.component.ts
+++ src/main/webapp/app/entities/table-of-prizes/table-of-prizes-update.component.ts
@@ -8,6 +8,8 @@ import { Observable } from 'rxjs';
 import { ITableOfPrizes, TableOfPrizes } from 'app/shared/model/table-of-prizes.model';
 import { TableOfPrizesService } from './table-of-prizes.service';
 import { IPrize, Prize } from 'app/shared/model/prize.model';
+import { ImageService } from '../image/image.service';
+import { IImage } from 'app/shared/model/image.model';
 
 @Component({
   selector: 'jhi-table-of-prizes-update',
@@ -18,19 +20,27 @@ export class TableOfPrizesUpdateComponent implements OnInit {
 
   editForm = this.fb.group({});
 
-  constructor(protected tableOfPrizesService: TableOfPrizesService, protected activatedRoute: ActivatedRoute, private fb: FormBuilder) {}
+  images: IImage[] = [];
+
+  constructor(
+    protected tableOfPrizesService: TableOfPrizesService,
+    protected imageService: ImageService,
+    protected activatedRoute: ActivatedRoute,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ tableOfPrizes }) => {
       this.createViewFromModel(tableOfPrizes);
     });
+    this.imageService.query().subscribe((res: HttpResponse<IImage[]>) => (this.images = res.body || []));
   }
 
   createViewFromModel(tableOfPrizes: ITableOfPrizes): void {
     const rows = this.fb.array([]);
     if (tableOfPrizes.prizes) {
-      for (let i = 0; i < tableOfPrizes.prizes.length; i++) {
-        rows.push(this.createViewRowFromModelChild(tableOfPrizes.prizes[i]));
+      for (const prize of tableOfPrizes.prizes) {
+        rows.push(this.createViewRowFromModelChild(prize));
       }
     }
     this.editForm = this.fb.group({
@@ -45,9 +55,7 @@ export class TableOfPrizesUpdateComponent implements OnInit {
     return this.fb.group({
       id: [prize.id],
       quantity: [prize.quantity, [Validators.required, Validators.min(1)]],
-      imageId: [prize.image?.id],
-      imageName: [prize.image?.name],
-      imageUrl: [prize.image?.url],
+      imageId: [prize.image?.id, [Validators.required]],
     });
   }
 
@@ -64,12 +72,23 @@ export class TableOfPrizesUpdateComponent implements OnInit {
   }
 
   private createModelFromView(): ITableOfPrizes {
+    const prizes: IPrize[] = [];
+    if (this.getViewRows()) {
+      for (const row of this.getViewRows().controls) {
+        prizes.push({
+          ...new Prize(),
+          id: row.get('id')!.value,
+          quantity: row.get('quantity')!.value,
+          image: this.findImageById(row.get('imageId')!.value),
+        });
+      }
+    }
     return {
       ...new TableOfPrizes(),
       id: this.editForm.get('id')!.value,
       name: this.editForm.get('name')!.value,
       userId: this.editForm.get('userId')!.value,
-      prizes: this.editForm.get('prizes')!.value,
+      prizes /* property shorthand */,
     };
   }
 
@@ -101,5 +120,20 @@ export class TableOfPrizesUpdateComponent implements OnInit {
 
   protected onSaveError(): void {
     this.isSaving = false;
+  }
+
+  trackId(index: number, item: ITableOfPrizes): number {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    return item.id!;
+  }
+
+  findImageById(id: number): IImage {
+    return this.images.find(i => i.id === id)!;
+  }
+
+  findImageUrlById(id: number): string | null {
+    const image = this.findImageById(id);
+    if (image && image.url) return image.url;
+    return null;
   }
 }
